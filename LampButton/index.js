7b8616e6f44ac3334bfdfd6a63eeaec149bb50dc
southlampw <southlampw@users.noreply.github.com>
2023-04-19 02:38:04 +0000

    deploy: a5f157dc20958dd98cb1031e4e7f85cc20016f5b

---
+++ LampButton/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Button } from 'antd';
+var LampButton = function LampButton() {
+  return /*#__PURE__*/React.createElement(Button, {
+    type: "primary"
+  }, "southlamp");
+};
+export default LampButton;
---
+++ index.d.ts
@@ -0,0 +1,1 @@
+export { default as LampButton } from './LampButton';
